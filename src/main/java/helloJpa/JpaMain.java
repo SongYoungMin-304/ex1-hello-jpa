397e69194d206fb010bd4640492a51ee4ada8533
82108 <dudals789@naver.com>
2023-02-06 21:58:13 +0900

    jpa 기본 세팅

---
+++ src/main/java/helloJpa/JpaMain.java
@@ -0,0 +1,47 @@
+package helloJpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+        //code
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+           /* Member member = new Member();
+            member.setId(1L);
+            member.setName("HelloA");*/
+
+            /*Member findMember = em.find(Member.class, 1L);
+            findMember.setName("HelloJpa");*/
+
+            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+                    .setFirstResult(1)
+                    .setMaxResults(10)
+                    .getResultList();
+
+            for (Member member : result) {
+                System.out.println("member.name = " + member.getName());
+            }
+
+
+            tx.commit();
+        }catch (Exception e){
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+}
